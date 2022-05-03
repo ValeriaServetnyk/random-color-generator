811750f549c936e676e91b96c1dc8cf26d17adc1
Valeria <Leraservetnik@gmail.com>
2022-05-03 22:33:57 +0200

    Add readme
    generated box of hashes with the random color or with the assigned color and luminosity

---
+++ README.md
@@ -0,0 +1,26 @@
+# Node.js random-color-generator
+
+Create an application that will return a random color in the command line.
+
+```bash
+$ node index.js
+shape 31x9# colored with a random color #ff0000
+
+```
+
+$node index.js red dark
+match for entered hue and luminosity
+
+###############################
+###############################
+###############################
+
+#####
+
+##### #930b46
+
+#####
+
+###############################
+###############################
+###############################
---
+++ index.js
@@ -0,0 +1,63 @@
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+import readline from 'readline';
+
+function squareFn(color) {
+  const squareArray = [];
+  let result;
+  for (let i = 0; i < 9; i++) {
+    result = '';
+    if (i === 3 || i === 5) {
+      for (let j = 0; j < 5; j++) {
+        result += '#';
+      }
+      for (let j = 0; j < 21; j++) {
+        result += ' ';
+      }
+      for (let j = 0; j < 5; j++) {
+        result += '#';
+      }
+    } else if (i === 4) {
+      for (let j = 0; j < 5; j++) {
+        result += '#';
+      }
+      for (let j = 0; j < 7; j++) {
+        result += ' ';
+      }
+      result += color;
+      for (let j = 0; j < 7; j++) {
+        result += ' ';
+      }
+      for (let j = 0; j < 5; j++) {
+        result += '#';
+      }
+    } else {
+      for (let j = 0; j < 31; j++) {
+        result += '#';
+      }
+    }
+
+    squareArray.push(result);
+  }
+  return squareArray.join('\n');
+}
+
+let colorStr;
+colorStr = process.argv[2];
+if (!colorStr) {
+  colorStr = 'random';
+}
+
+let colorLumi;
+colorLumi = process.argv[3];
+if (!colorLumi) {
+  colorLumi = 'random';
+}
+
+const color = randomColor({
+  luminosity: colorLumi,
+  hue: colorStr,
+});
+
+console.log(chalk.hex(`${color}`).bold(`${squareFn(color)}`));
+console.log(colorLumi);
